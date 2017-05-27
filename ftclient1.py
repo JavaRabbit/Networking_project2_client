8ffe54a421c13260cbc6d8c49b517a3e20eaef37
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-26 18:32:34 -0700

    working on client, able to connect

--- ftclient1.py
+++ ftclient1.py
@@ -1,4 +1,18 @@
 # Networking Project 2
 # Client in Python using 2.7.5
 
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 50050
+
+
+s.connect((host,port))
+print s.recv(1024)
+
+
+
+#close the connection
+s.close
 print "hello"
