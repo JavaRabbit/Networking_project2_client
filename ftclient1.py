ffa83b2f3c8b4d314062c2e9a81d65778439d52c
Bonnie <kwongb@flip1.engr.oregonstate.edu>
2017-05-27 14:39:27 -0700

    added numArguments on command line check

--- ftclient1.py
+++ ftclient1.py
@@ -2,10 +2,38 @@
 # Client in Python using 2.7.5
 
 import socket
+import sys
+import signal
+import os
+
+
+######################################
+# Function to verify command line arguments
+# verify that there are 5 arguments and that usage is correct
+# else print usage to user and exit
+def verifyNumArguments():
+  if len(sys.argv) != 5:
+    print "Usage: ftclient flip1 30021 -l 30020"
+    exit(1)
+
+
+
+# Verify user entered correct number of arguments
+verifyNumArguments() 
+
+
+
+
+
 
 s = socket.socket()
 host = socket.gethostname()
-port = 50050
+#port = 50050
+
+
+# Client usage  ftclient flip1 30021 -l  30021
+port = int(sys.argv[2])
+
 
 
 s.connect((host,port))
